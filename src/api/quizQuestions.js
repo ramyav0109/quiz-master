cc76bad8e77cdcf3590ef6536ee5c04948a4e7ad
ramyav0109 <ramyav0109@users.noreply.github.com>
2017-10-05 13:27:28 -0400

    Initial commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,46 @@
+var quizQuestions = [
+  {
+      question: "What is Lorem Ipsum...",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Yes",
+              score: 1
+          },
+          {
+              type: "Nintendo",
+              content: "No",
+              score: 2
+          }     
+      ]
+  },
+  {
+      question: "Where does it come from?",
+      description: "Contray to popular belief, Lorem Ipsum is not simply random text. It has roots in aplace of classical Latin literature from 45 BC, making it over 2000 years old.",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Whatever!",
+              score: 1
+          },
+          {
+              type: "Nintendo",
+              content: "Ohk!!",
+              score: 2
+          },
+          {
+              type: "Sony",
+              content: "Hmmm...Maybe",
+              score: 3
+          },
+          {
+              type: "Forza",
+              content: "Yeah!",
+              score: 4
+          }
+        ]
+  }
+]
+
+export default quizQuestions;
